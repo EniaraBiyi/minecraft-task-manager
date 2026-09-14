f66a25ebd6f010ef7d2c22361a0ff77eda8c6c46
EniaraBiyi <biyiolutundee@gmail.com>
2026-09-14 20:43:52 +0100

    commands.py restructure

--- commands.py
+++ commands.py
@@ -71,44 +71,17 @@ def remtask(tasks):
                 break
         print("\nThe task was successfully deleted\n")
 
-def toggle_status(tasks_in_session, current_status):
-    matching_tasks = [task.get("name") for task in tasks_in_session if task.get("status") == current_status]
+def checktask(tasks):
+    h.toggle_status(tasks, False)
 
-    status = "completed" if current_status else "uncompleted"
-    action = "reverted to uncompleted" if current_status else "marked as completed"
+def unchecktask(tasks):
+    h.toggle_status(tasks, True)
 
-    if not matching_tasks:
-        print(f"There are no {status} tasks as of now\n")
-        return
+def taskcomp(tasks):
+    h.tasks_by_status(tasks, True, "Completed")
 
-    print(f"\nHere are your {status} tasks: ")
-
-    for task in matching_tasks:
-        print(task)
-
-    task_to_toggle = input(f"\nEnter the name of the task which you wish to have {action}: ").strip().lower()
-
-    if not task_to_toggle in matching_tasks:
-        print(f"The input you entered is invalid. It is not a task that can be {action}\n")
-        return
-
-    else:
-        for task in tasks_in_session:
-            if task.get("name") == task_to_toggle:
-                task["status"] = not current_status
-                print(f"\nThe task was successfully {action}\n")
-                break
-
-def tasks_by_status(list_of_tasks, status, label):
-    matching_tasks = [task for task in list_of_tasks if task.get("status") == status]
-    if not matching_tasks:
-        print(f"You have no {label} tasks")
-        return
-
-    print(f"{label} tasks: ")
-    for task in matching_tasks:
-        print(task.get("name"))
-    print()
+def taskluncomp(tasks):
+    h.tasks_by_status(tasks, False, "Uncompleted")
 
 def searchtask(tasks):
     if not tasks:
@@ -160,89 +133,32 @@ def alltasks(tasks):
 
     print()
 
+def addplayers(players):
+    h.add_items(players, "player", "players")
 
-def add_items(item_list, item_type, item_type_plural):
-    print(f"\nEnter {item_type_plural} you wish to add, or press Enter alone to finish:")
+def remplayers(players):
+    h.remove_items(players, "player", "players")
 
-    added_items = []
-    while True:
-        item = input()
+def listplayers(players):
+    h.list_items(players, "players")
 
-        if item:
-            if item.isspace():
-                print(f"This is all whitespaces and isn't a valid {item_type}\n"
-                      f"Enter valid {item_type_plural}, or press Enter to stop")
-                continue
-            if item.strip().lower() not in item_list:
-                item_list.append(item.strip().lower())
-                added_items.append(item.strip().lower())
-            else:
-                print(
-                    f"This {item_type} is already saved in this session. Be mindful of spelling. Casing is irrelevant")
-                print("You may continue, or press Enter to finish:")
-                continue
+def addlocations(locations):
+    h.add_items(locations, "location", "locations")
 
-        else:
-            break
+def remlocations(locations):
+    h.remove_items(locations, "location", "locations")
 
-    label = item_type if len(added_items) == 1 else item_type_plural
-    verb = "was" if len(added_items) == 1 else "were"
-    print(f"{len(added_items)} {label} {verb} added")
+def listlocations(locations):
+    h.list_items(locations, "locations")
 
-    if added_items:
-        print(f"Added {item_type_plural}:")
-        for item in added_items:
-            print(item)
-    print()
+def addcategories(categories):
+    h.add_items(categories, "category", "categories")
 
+def remcategories(categories):
+    h.remove_items(categories, "category", "categories", "default")
 
-def remove_items(item_list, item_type, item_type_plural, protected_item=None):
-    min_length = 1 if protected_item else 0
-
-    if len(item_list) == min_length:
-        print(f"There aren't any {item_type_plural} saved in this session that can be deleted\n")
-        return
-
-    print(f"\nEnter {item_type_plural} you wish to remove, or press Enter alone to finish:")
-
-    removed_items = []
-    while True:
-        item = input()
-
-        if item and item.strip().lower() in item_list:
-            if item.strip().lower() != protected_item:
-                item_list.remove(item.strip().lower())
-                removed_items.append(item.strip().lower())
-            else:
-                print(f"{protected_item} is a built-in {item_type} for tasks. It cannot be removed\n"
-                      "You may continue, or press Enter to finish:")
-                continue
-        elif item and item.strip().lower() not in item_list:
-            print(f"This {item_type} is not saved to the system. Be mindful of spelling. Casing is irrelevant")
-            print("You may continue, or press Enter to finish:")
-            continue
-        elif not item:
-            break
-
-    label = item_type if len(removed_items) == 1 else item_type_plural
-    verb = "was" if len(removed_items) == 1 else "were"
-    print(f"{len(removed_items)} {label} {verb} removed")
-
-    if removed_items:
-        print(f"Removed {item_type_plural}:")
-        for item in removed_items:
-            print(item)
-    print()
-
-def list_items(list_of_items, list_item_plural):
-    if not list_of_items:
-        print(f"There are no {list_item_plural} saved yet\n")
-
-    else:
-        print(f"\nHere are the {list_item_plural} saved in your session:")
-        for item in list_of_items:
-            print(item)
-        print()
+def listcategories(categories):
+    h.list_items(categories, "categories")
 
 def help_message():
     print("-The program manages all your TASKS. Tasks are the work you need to get done\n"
@@ -311,5 +227,27 @@ def allstatus(tasks, players, locations, categories):
         return
 
     else:
-        tasks_by_status(tasks, True, "Completed")
-        tasks_by_status(tasks, False, "Uncompleted")
+        h.tasks_by_status(tasks, True, "Completed")
+        h.tasks_by_status(tasks, False, "Uncompleted")
+
+
+def setup(item_list, item_type, item_type_plural, presaved=0):
+    print(f"Enter {item_type_plural} you want to save, or press Enter with no input to finish:")
+    while True:
+        item = input()
+        if item:
+            if item.isspace():
+                print(f"I don't see anything. Please enter a valid {item_type}")
+                continue
+            elif item.lower().strip() in item_list:
+                print(f"You already entered this {item_type}")
+                continue
+            item_list.append(item.strip().lower())
+        else:
+            if len(item_list) == presaved:
+                print(f"\nNo new {item_type_plural} were entered")
+                break
+            print(f"\nThese are the {item_type_plural} you have entered: ")
+            for item in item_list:
+                print(item)
+            break
